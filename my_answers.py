6ce3508474271a1cb3c195ca342fed6b3fcc05a3
mshokry <msn4eg@gmail.com>
2018-07-07 22:56:17 +0200

    Time to GPU CPU not working :(

--- my_answers.py
+++ my_answers.py
@@ -12,7 +12,9 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    for i in range(len(series)-window_size):
+        X.append(series[i:i+window_size])
+    y=series[window_size:]
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,13 +25,23 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1,activation='linear'))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
-
+    punctuation = ['!', ',', '.', ':', ';', '?', ' ']
+    for l in range(0,97): #all before a
+        ll = chr(l)
+        if ll not in(punctuation):
+            text = text.replace(ll, ' ')
+    for l in range(123,255): #all after z
+        ll = chr(l)
+        if ll not in(punctuation):
+            text = text.replace(ll, ' ')
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +49,15 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    for i in range(0,len(text)-window_size,step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size,num_chars)))
+    model.add(Dense(num_chars,activation='softmax'))
+    return model
